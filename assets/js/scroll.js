99287bbe52c504e6ffddc81abec786a17a6d473e
monikinhanx <craveiromonica@gmail.com>
2018-12-09 01:30:01 -0200

    Adicionando Patrocinadores, corrigindo bugs e efeito scroll e scrollspy

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -9,3 +9,40 @@ function scrollBanner() {
 }
 
 window.addEventListener('scroll', scrollBanner);
+
+$(document).ready(() => {
+    $('.scrollSuave').click(function (e) {
+        e.preventDefault();
+        var target = this.hash,
+            $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 1000, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+});
+
+$(document).ready(() => {
+    $('.toTop').click(function (e) {
+        e.preventDefault();
+        var target = this.hash,
+            $target = $(target);
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000, 'linear');
+    });
+});
+
+// $('html, body').animate({
+//     scrollTop: 0
+// }, 1000, 'linear');
+//     }
+
+// var $doc = $('html, body');
+// $('.scrollSuave').click(function () {
+//     $doc.animate({
+//         scrollTop: $($.attr(this, 'href')).offset().top
+//     }, 1000);
+//     return false;
+// });
